73b32e9b2fe71b6e73d34f109b756efb839b6c4f
kwonk <kwkim.korea@gmail.com>
2019-01-25 00:00:24 +0900

    no message

---
+++ Source/Library.py
@@ -0,0 +1,66 @@
+# Ignore the warnings
+import warnings
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+
+# System related and data input controls
+import os
+import glob
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
+
+# Data manipulation and visualization
+import pandas as pd
+pd.options.display.float_format = '{:,.2f}'.format
+pd.options.display.max_rows = 1000
+pd.options.display.max_columns = 1000
+import numpy as np
+import itertools
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.mlab as mlab
+from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import StrMethodFormatter
+from matplotlib.ticker import PercentFormatter
+import seaborn as sns
+import missingno as msno
+
+# Modeling algorithms
+# General
+import statsmodels.api as sm
+from scipy import stats
+
+# Regression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor, GradientBoostingRegressor, AdaBoostRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+
+# Classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+# Time series
+import arch
+from pyramid.arima import auto_arima
+
+# Model selection
+from sklearn.model_selection import train_test_split,cross_validate
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+# Evaluation metrics
+# for regression
+from sklearn.metrics import mean_squared_log_error, mean_squared_error,  r2_score, mean_absolute_error
+# for classification
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
